e87b7b101692ddb2f0863168bdd0b3ad7e438e88
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-07-07 22:42:26 -0700

    addded ref

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=6_N8QZ47toY&list=PL4rzdwizLaxYmltJQRjq18a9gsSyEQQ-0&index=1
+
 public class MainClass {
     public static void main(String[] args) {
         new Game();
